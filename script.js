584f6df5a9b2997ee0ab3be71c46aeda8d8be420
Rajiv Dubey <161409149+RAJIV81205@users.noreply.github.com>
2024-11-17 17:35:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   navLinks.forEach(link => {
     const linkText = link.textContent.trim(); // Get the text inside the anchor tag
     const linkHref = link.getAttribute("href");
+    const linkHref1 = linkHref.spilt(".");
 
     // Match the text or href with the current page
-    if (currentPage.includes(linkHref) || linkHref === "#") {
+    if (currentPage.includes(linkHref1) || linkHref === "#") {
       link.parentElement.classList.add("active-box"); // Add class to the parent div for styling
     } else {
       link.parentElement.classList.remove("active-box"); // Ensure others are not active
